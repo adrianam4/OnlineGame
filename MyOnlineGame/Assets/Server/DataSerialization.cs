b9c8a1710ce0b5ab9999973a60e1be8e93f1862d
unaidiaz <61042480+unaidiaz@users.noreply.github.com>
2022-11-10 09:24:20 +0100

    Revert "puta"
    This reverts commit 12cf8edea15edd979c47f5f317d7c5c2d6c26ff3.

--- MyOnlineGame/Assets/Server/DataSerialization.cs
+++ MyOnlineGame/Assets/Server/DataSerialization.cs
@@ -13,7 +13,6 @@ public class DataSerialization : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         newRotation=Quaternion.identity;
         newPosition = new Vector3();
         byte[] data = new byte[100];
