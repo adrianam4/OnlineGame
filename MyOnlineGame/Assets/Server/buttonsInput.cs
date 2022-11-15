971c6fce3d1879a4146db481fa497c679ea7e988
Adrian Aroca Mascaraque <61042878+adrianam4@users.noreply.github.com>
2022-11-15 18:45:51 +0100

    find player

--- MyOnlineGame/Assets/Server/buttonsInput.cs
+++ MyOnlineGame/Assets/Server/buttonsInput.cs
@@ -20,6 +20,7 @@ public class buttonsInput : MonoBehaviour
     public GameObject UDPserver;
     public GameObject UDPclient;
     public GameObject changeTypeObject;
+    public GameObject player;
     Change_Type change;
     string inputText;
     string conectionType="-";
@@ -109,10 +110,10 @@ public class buttonsInput : MonoBehaviour
     void playTaskOnClick()
     {
 
-        if(UDPserver.GetComponent<UDP_Server>().client != null)
+        if(UDPserver.GetComponent<UDP_Server>().client != null || UDPclient.GetComponent<UDP_Client>().server != null)
         {
             GameObject.Find("Canvas").SetActive(false);
-            GameObject.Find("Player").SetActive(true);
+            player.SetActive(true);
         }
         
     }
