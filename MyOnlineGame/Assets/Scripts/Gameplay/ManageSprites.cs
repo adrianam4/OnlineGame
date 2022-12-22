93a2904a2610afdfcbbf69c63935d9beec398b45
unaidiaz <61042480+unaidiaz@users.noreply.github.com>
2022-12-22 09:44:53 +0100

    ff

--- MyOnlineGame/Assets/Scripts/Gameplay/ManageSprites.cs
+++ MyOnlineGame/Assets/Scripts/Gameplay/ManageSprites.cs
@@ -27,46 +27,46 @@ public class ManageSprites : MonoBehaviour
 
         if (clientCount == 0)
         {
-            player1.SetActive(false);
+            //player1.SetActive(false);
             player1UI.SetActive(false);
 
-            player2.SetActive(false);
+            //player2.SetActive(false);
             player2UI.SetActive(false);
 
-            player3.SetActive(false);
+            //player3.SetActive(false);
             player3UI.SetActive(false);
         }
         else if (clientCount == 1)
         {
-            player1.SetActive(true);
+            //player1.SetActive(true);
             player1UI.SetActive(true);
 
-            player2.SetActive(false);
+            //player2.SetActive(false);
             player2UI.SetActive(false);
 
-            player3.SetActive(false);
+            //player3.SetActive(false);
             player3UI.SetActive(false);
         }
         else if (clientCount == 2)
         {
-            player1.SetActive(true);
+            //player1.SetActive(true);
             player1UI.SetActive(true);
 
-            player2.SetActive(true);
+            //player2.SetActive(true);
             player2UI.SetActive(true);
 
-            player3.SetActive(false);
+            //player3.SetActive(false);
             player3UI.SetActive(false);
         }
         else
         {
-            player1.SetActive(true);
+            //player1.SetActive(true);
             player1UI.SetActive(true);
 
-            player2.SetActive(true);
+            //player2.SetActive(true);
             player2UI.SetActive(true);
 
-            player3.SetActive(true);
+            //player3.SetActive(true);
             player3UI.SetActive(true);
         }
     }
